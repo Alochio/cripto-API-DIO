0d029cbc7278fb7b62dedbaf9051a71dbbba7eb5
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-06 21:38:42 -0300

    api funcionando - faltando estilização

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Projeto Web utilizando a API Coin Market Cap <br>
 
 conta criada: https://pro.coinmarketcap.com/account/
 extensão nescessária: Moesif CORS
+
+
+link incorporar img: https://s2.coinmarketcap.com/static/img/coins/64x64/3.png
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,18 +14,27 @@ fetch(
     return response.json();
   })
   .then((api) => {
-
+    console.log(api);
     var texto = "";
     for (let i = 0; i < 10; i++) {
+
+
+    const dataString = api.data[i].first_historical_data;
+    const data = new Date(dataString);
+    const firstHistoricalDataFormated = `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+
+
+
     
       texto =
         texto +
         `
             <div class="media">
-                <img src="./coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+                <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/${api.data[i].id}.png" class="align-self-center mr-3 imagem" alt="coin">
                 <div class="media-body">
-                <h5 class="mt-2">${api.data[i].name}</h5>
+                <h5 class="mt-2 name">${api.data[i].name}</h5>
                 <p>${api.data[i].symbol}</p>
+                <p>Data Histórica Inicial: <span id="data-formatada-${i}">${firstHistoricalDataFormated}</span></p>
                 </div>
             </div>
             `;
